b74e559f5fcbddb38b07654a18158d7e481204fb
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-20 14:49:59 +0700

    :money_with_wings: Refactor Services && Routes

--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from config.db import get_db
-from models.index import users
 from schemas.user import *
 from services.index import *
 
@@ -12,52 +11,25 @@ router = APIRouter(
 
 
 @router.get("/users/list")
-async def read_user_all(db: Session = Depends(get_db)):
-    return user_all(db=db)
+async def get_all(db: Session = Depends(get_db)):
+    return get_user_all(db=db)
 
 
 @router.get("/user/get/{id}")
-async def read_user_byId(id: int, db: Session = Depends(get_db)):
-    return db.query(users).filter(users.id == id).first()
+async def get_byId(id: int, db: Session = Depends(get_db)):
+    return get_user_byId(id=id, db=db)
 
 
 @router.post("/user/add", status_code=status.HTTP_201_CREATED)
-async def create_user(user: RequestUser, db: Session = Depends(get_db)):
-    db_user = users(
-        email=user.email,
-        username=user.username,
-        password=user.password,)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return {"status": "success", "data": db_user}
+async def create(user: RequestUser, db: Session = Depends(get_db)):
+    return create_user(user=user, db=db)
 
 
 @router.put("/user/update/{id}")
-async def update_user(id: int, user: RequestUser, db: Session = Depends(get_db)):
-    if id is None:
-        raise HTTPException(status_code=400, detail="id is required")
-    db_user = db.query(users).filter(users.id == id).first()
-    if db_user is None:
-        raise HTTPException(status_code=404, detail=f'User id {id} not found')
-    if db.query(users).filter(users.email == user.email).first() and user.email != db_user.email:
-        raise HTTPException(status_code=400, detail="Email already exists")
-    db_user.username = user.username
-    db_user.email = user.email
-    db_user.password = user.password
-    db_user.role = user.role
-    db.commit()
-    db.refresh(db_user)
-    return {"status": "success", "data": db_user}
+async def update(id: int, user: RequestUser, db: Session = Depends(get_db)):
+    return update_user(id=id, user=user, db=db)
 
 
 @router.delete("/user/delete/{id}")
 async def delete_user(id: int, db: Session = Depends(get_db)):
-    if id is None:
-        raise HTTPException(status_code=400, detail="id is required")
-    db_user = db.query(users).filter(users.id == id).first()
-    if db_user is None:
-        raise HTTPException(status_code=404, detail=f'User id {id} not found')
-    db.delete(db_user)
-    db.commit()
-    return {"status": "success"}
+    return delete_user(id=id, db=db)
--- services/user.py
+++ services/user.py
@@ -1,10 +1,52 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from config.db import get_db
 from models.index import users
 from schemas.index import *
 
 
-def user_all(db: Session = Depends(get_db)):
+def get_user_all(db: Session = Depends(get_db)):
     rows = db.query(users).order_by(users.id.desc()).all()
-    return {"status": "success", "data": rows}
+    return {"status": "success", "payload": rows}
+
+
+def get_user_byId(id: int, db: Session = Depends(get_db)):
+    rows = db.query(users).filter(users.id == id).first()
+    return {"status": "success", "payload": rows}
+
+
+def create_user(user: RequestUser, db: Session = Depends(get_db)):
+    payload = users(
+        email=user.email,
+        username=user.username,
+        password=user.password,)
+    db.add(payload)
+    db.commit()
+    db.refresh(payload)
+    return {"status": "success", "data": payload}
+
+def update_user(id: int, user: RequestUser, db: Session = Depends(get_db)):
+    if id is None:
+        raise HTTPException(status_code=400, detail="id is required")
+    payload = db.query(users).filter(users.id == id).first()
+    if payload is None:
+        raise HTTPException(status_code=404, detail=f'User id {id} not found')
+    if db.query(users).filter(users.email == user.email).first() and user.email != payload.email:
+        raise HTTPException(status_code=400, detail="Email already exists")
+    payload.username = user.username
+    payload.email = user.email
+    payload.password = user.password
+    payload.role = user.role
+    db.commit()
+    db.refresh(payload)
+    return {"status": "success", "payload": payload}
+
+def delete_user(id: int, db: Session = Depends(get_db)):
+    if id is None:
+        raise HTTPException(status_code=400, detail="id is required")
+    payload = db.query(users).filter(users.id == id).first()
+    if payload is None:
+        raise HTTPException(status_code=404, detail=f'User id {id} not found')
+    db.delete(payload)
+    db.commit()
+    return {"status": "success"}
